56edf830727ea3b38b98ee611d46996d2b5c0e90
Artyom O <aoleynichenko@taxdome.com>
2019-11-12 16:02:45 +0200

    ...

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://cdn.jsdelivr.net/gh/temofart/eva/dist'
-    : '/',
-  configureWebpack: {
-    output: {
-      filename: 'app.js',
-    },
-    optimization: {
-      splitChunks: false
-    },
-  }
+  publicPath: 'https://cdn.jsdelivr.net/gh/temofart/eva/dist'
 }
